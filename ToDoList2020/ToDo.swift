8e5f588fdee08313cd4a012348082f891ed1876e
kmorganfield <kennedymorganfield@gmail.com>
2020-05-30 12:39:43 -0500

    Iteration 0 complete

---
+++ ToDoList2020/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoList2020
+//
+//  Created by Kennedy Morganfield on 5/30/20.
+//  Copyright © 2020 Kennedy Morganfield. All rights reserved.
+//
+
+class ToDo {
+    var name = ""
+    var important = false
+}
+
