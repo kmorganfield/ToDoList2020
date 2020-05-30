707d7a5b64d63f18f49ce5a5a3d98ff31fa694db
kmorganfield <kennedymorganfield@gmail.com>
2020-05-30 11:54:41 -0500

    Initial commit

---
+++ ToDoList2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList2020
+//
+//  Created by Kennedy Morganfield on 5/30/20.
+//  Copyright © 2020 Kennedy Morganfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
